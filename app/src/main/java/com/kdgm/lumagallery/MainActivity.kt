66722b945709611c5e235d3c0f146f6124ee6828
Gayan Madhusanka <gayanm@digital400.com>
2026-02-06 12:32:30 +0530

    chore: set up app foundation and project structure

--- app/src/main/java/com/kdgm/lumagallery/MainActivity.kt
+++ app/src/main/java/com/kdgm/lumagallery/MainActivity.kt
@@ -16,32 +16,10 @@ import com.kdgm.lumagallery.ui.theme.LumaGalleryTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             LumaGalleryTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AppRoot()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LumaGalleryTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/kdgm/lumagallery/ui/AppRoot.kt
@@ -0,0 +1,12 @@
+package com.kdgm.lumagallery.ui
+
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AppRoot() {
+    Surface {
+        // Navigation will live here later
+    }
+}
+
